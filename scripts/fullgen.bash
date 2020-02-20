be4ee006dfd9d2731ce4f75c9d4dddde604d4533
Malte Kießling <mkalte@mkalte.me>
2020-02-20 22:38:52 +0100

    fullgen clears pycache

--- scripts/fullgen.bash
+++ scripts/fullgen.bash
@@ -7,6 +7,7 @@ fi
 
 includeDir=$1
 
+rm -rf ./scripts/__pycache__/
 ./scripts/generator.py "$includeDir" ./include/sdl2wrap/detail/generated/
 ./scripts/buildFeaturelist.bash "$includeDir" features.md
 ./scripts/format.bash .
